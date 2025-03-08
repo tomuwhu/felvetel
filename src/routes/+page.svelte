de21dd6928c7525b8d2a05fd2637dc7c95ba73e7
Dr. Németh Tamás <tomuwhu@gmail.com>
2025-03-08 14:02:52 +0100

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,5 @@
 <script>
-  var i = $state(0)
   let { data } = $props() 
 </script>
-<h1>Eszköz nyilvántartó program</h1>
-<a 
-  class="ui blue button"
-  href="/cica">Kattints ide</a>
-<h2>{i}</h2>
+<h1>Eszköz-nyilvántartó program</h1>
 <h3>{data.x}</h3>
